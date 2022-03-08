d7b30c055bb2b6b2cd4c7fa106db5de90354cb9a
zaher-aa <zaherabuamro@gmail.com>
2022-03-08 19:19:26 +0200

    Added routes for router floder Relates #28

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
-const {handleRegister} = require('../controllers');
+
+const {
+  handleRegister,
+  loginPageHandler,
+  handleLogin,
+  homePageHandler,
+} = require('../controllers');
+
+const {
+  checkAuth,
+} = require('../middleware');
 
 router.get('/register', handleRegister);
+router.get('/login', loginPageHandler);
+
+router.post('/login', handleLogin);
+router.get('/home', checkAuth, homePageHandler);
 
 module.exports = router;
