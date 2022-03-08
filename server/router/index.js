5c0cf3552b4c55b17c318ccedc2058a3afe2a66b
yousra Jaradh <yousrakhaleel@gmail.com>
2022-03-08 14:20:59 +0300

    handle register page Relates #27

--- server/router/index.js
+++ server/router/index.js
@@ -1,4 +1,8 @@
+const { join } = require('path');
 const router = require('express').Router();
 
+router.get('/register', (req, res) => {
+    res.sendFile(join(__dirname, '..', '..', 'views', 'html', 'register.html'));
+});
 
 module.exports = router;
