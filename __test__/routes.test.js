6b47864917ca024d6771deda0de6a17d7844a983
yousra Jaradh <yousrakhaleel@gmail.com>
2022-03-09 18:09:26 +0300

    test home page

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -1,3 +1,15 @@
-test('', () => {
-  expect(1).toBe(1);
-})
+const request = require('supertest');
+const app = require('../server/app');
+
+describe('Test server home page', () => {
+  test('GET Route /, status 200, content-type html', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
