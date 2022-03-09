f4ef0bae2454d212e10927c2ccf52144985fb5b0
mayar-kabaja <kabajamayar@gmail.com>
2022-03-09 14:46:39 +0200

    create database test for signupDB

--- __test__/db.test.js
+++ __test__/db.test.js
@@ -1,3 +1,30 @@
-test('', () => {
-  expect(1).toBe(1);
-})
+const connection = require('../server/database/config/connections');
+const { dbBuild } = require('../server/database/config/build');
+const signUpQuirey = require('../server/database/queries/signupDB');
+
+beforeEach(() => dbBuild());
+
+test('Insert  user  in DB and check your email ', () => {
+  const data = {
+    name: 'Mayar',
+    email: 'mayar@gmail.com',
+    password: '5dywOrzop4bONVjqcjOU0mtInkvJDG5yoyXxDeWAZqSWou0Z3O',
+  };
+  return signUpQuirey(data).then((data) => {
+    console.log(data.rows[0]);
+    expect(data.rows[0]).toEqual({ email: 'mayar@gmail.com' });
+  });
+});
+
+test('Insert  user  in DB and check your email ', () => {
+  const data = {
+    name: 'Ali',
+    email: 'ali@gmail.com',
+    password: '5dywOrzop4bONVjqcjOU0mtInkvJDG5yoyXxDeWAZqSWou0Z3O',
+  };
+  return signUpQuirey(data).then((data) => {
+    console.log(data.rows[0]);
+    expect(data.rows[0]).toEqual({ email: 'ali@gmail.com' });
+  });
+});
+afterAll(() => connection.end());
--- server/database/config/connections.js
+++ server/database/config/connections.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 let dbUrl = '';
 
-if (process.env.NODE_ENV === 'test') dbUrl = process.env.DB_URL_test;
 if (process.env.NODE_ENV === 'dev') dbUrl = process.env.DB_URL;
 if (process.env.NODE_ENV === 'production') dbUrl = process.env.DATABASE_URL;
+if (process.env.NODE_ENV === 'test') dbUrl = process.env.DB_URL_test;
 
 if (!dbUrl) throw new Error('NO DB_URL');
 
