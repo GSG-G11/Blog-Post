7c8ac024e2c3e546751eee4e80e34d87e66a7b79
zaher-aa <zaherabuamro@gmail.com>
2022-03-09 13:08:21 +0200

    Added needed database queries Relates #43

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,9 +1,15 @@
 const getUserInfo = require('./getUseInfo');
 const getUsernameQuery = require('./getUsernameQuery');
 const postBlogDB = require('./postBlogDB');
+const getAllBlogsQuery = require('./getAllBlogsQuery');
+const getUserNameUponIdQuery = require('./getUserNameUponIdQuery');
+const getUserIdQuery = require('./getUserIdQuery');
 
 module.exports = {
   getUserInfo,
   getUsernameQuery,
   postBlogDB,
+  getAllBlogsQuery,
+  getUserNameUponIdQuery,
+  getUserIdQuery,
 };
