d5fce4111a33b88369cd41a1bed0afe3169e6bd9
zaher-aa <zaherabuamro@gmail.com>
2022-03-09 13:09:44 +0200

    Added additional routes Realtes #43

--- server/router/index.js
+++ server/router/index.js
@@ -8,15 +8,19 @@ const {
   postBlog,
   logoutUser,
   getUserName,
+  getAllBlogs,
+  getUsernameBasedOnId,
 } = require('../controllers');
 const { checkAuth } = require('../middleware');
 
 router.get('/register', handleRegister);
 router.get('/login', loginPageHandler);
 router.get('/home', checkAuth, homePageHandler);
-router.post('/username', getUserName);
+router.get('/all-blogs', getAllBlogs);
+router.get('/username', getUsernameBasedOnId);
+router.post('/username', checkAuth, getUserName);
 router.post('/logout', logoutUser);
 router.post('/login', handleLogin);
-router.post('/add-blog', postBlog);
+router.post('/add-blog', checkAuth, postBlog);
 
 module.exports = router;
