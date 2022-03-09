d7201f76a6598b000f00a42a02352dbe0464b065
yousra Jaradh <yousrakhaleel@gmail.com>
2022-03-09 18:17:57 +0300

    test not found page

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../server/app');
 
-describe('Test server home page', () => {
+describe('Test route', () => {
   test('GET Route /, status 200, content-type html', (done) => {
     request(app)
       .get('/')
@@ -12,4 +12,17 @@ describe('Test server home page', () => {
         return done();
       });
   });
+
+test('GET Route /xx, status 404, content-type html', (done) => {
+  request(app)
+    .get('/xx')
+    .expect(404)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      if (err) return done(err);
+      return done();
+    });
+});
+
+
 });
--- server/controllers/pageNotFoundError.js
+++ server/controllers/pageNotFoundError.js
@@ -4,7 +4,7 @@ const serverError = require('./serverError')
 module.exports = (req, res, next) => {
   res
     .status(404)
-    .sendFile(join(__dirname, '..', '..', 'views', 'html', '404.html22'), (err) => {
+    .sendFile(join(__dirname, '..', '..', 'views', 'html', '404.html'), (err) => {
       if (err) {
         res
           .status(500)
