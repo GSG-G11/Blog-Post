d75b432a1ce9059612c2135189d0e2c1541a1c2f
zaher-aa <zaherabuamro@gmail.com>
2022-03-09 13:09:26 +0200

    Edited checkAuth.js middleware to be generic Relates #43

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -1,19 +1,15 @@
 const { verify } = require('jsonwebtoken');
-const { getUsernameQuery } = require('../database/queries');
 
 module.exports = (req, res, next) => {
   const { access_token: token } = req.cookies;
 
   if (token) {
-    verify(token, process.env.SECRET, (err, encoded) => {
+    verify(token, process.env.SECRET, (err, decoded) => {
       if (err) {
         res.redirect('/login');
       } else {
-        getUsernameQuery(encoded.email)
-          .then(() => {
-            next();
-          })
-          .catch(() => res.redirect('/login'));
+        req.decoded = decoded;
+        next();
       }
     });
   } else res.redirect('/login');
