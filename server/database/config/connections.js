358eff167f1d74cc36b8420dee8d3e6aa00af583
mayar-kabaja <kabajamayar@gmail.com>
2022-03-09 17:00:11 +0200

    fix testDB

--- server/database/config/connections.js
+++ server/database/config/connections.js
@@ -5,7 +5,7 @@ let dbUrl = '';
 
 if (process.env.NODE_ENV === 'dev') dbUrl = process.env.DB_URL;
 else if (process.env.NODE_ENV === 'production') dbUrl = process.env.DATABASE_URL;
-else if (process.env.NODE_ENV === 'test') dbUrl = process.env.DB_URL_TEST;
+else if (process.env.NODE_ENV === 'test') dbUrl = process.env.DB_URL_test;
 else throw new Error('NO DB_URL');
 
 const options = {
