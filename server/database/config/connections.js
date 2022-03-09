0b03a117d71d940c3a11839f0c88c96ace722824
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-09 19:01:55 +0200

    Resolve github actions database connection

--- server/database/config/connections.js
+++ server/database/config/connections.js
@@ -4,12 +4,17 @@ require('dotenv').config();
 let dbUrl = '';
 
 if (process.env.NODE_ENV === 'dev') dbUrl = process.env.DB_URL;
-else if (process.env.NODE_ENV === 'production') { dbUrl = process.env.DATABASE_URL; } else if (process.env.NODE_ENV === 'test') dbUrl = process.env.DB_URL_test;
+else if (process.env.NODE_ENV === 'production') {
+  dbUrl = process.env.DATABASE_URL;
+} else if (process.env.NODE_ENV === 'test') dbUrl = process.env.DB_URL_test;
 else throw new Error('NO DB_URL');
 
 const options = {
   connectionString: dbUrl,
-  ssl: { rejectUnauthorized: false },
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 };
 
 module.exports = new Pool(options);
