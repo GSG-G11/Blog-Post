38d93fd70b8063163c2d9054bb36583864c83191
yousra Jaradh <yousrakhaleel@gmail.com>
2022-03-09 17:34:10 +0300

    edit qoute

--- views/js/home.js
+++ views/js/home.js
@@ -1,23 +1,23 @@
-const logoutBtn = document.querySelector(".logout");
+const logoutBtn = document.querySelector('.logout');
 const addBlogForm = document.forms[0];
-const blogsContainer = document.querySelector(".blogs .container");
-const welcome = document.querySelector(".welcome");
+const blogsContainer = document.querySelector('.blogs .container');
+const welcome = document.querySelector('.welcome');
 
 const deletepost = (id) =>
   fetch(`/delete-Post/${id}`, {
-    method: "DELETE",
+    method: 'DELETE',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
 const createDeleteBtn = (blogHeader) => {
-  const deleteBlog = document.createElement("span");
-  deleteBlog.className = "delete";
-  deleteBlog.textContent = "Delete";
+  const deleteBlog = document.createElement('span');
+  deleteBlog.className = 'delete';
+  deleteBlog.textContent = 'Delete';
   blogHeader.appendChild(deleteBlog);
 };
-document.addEventListener("click", (e) => {
-  if (e.target.matches(".delete")) {
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.delete')) {
     e.target.parentElement.parentElement.remove();
     const blogId = e.target.parentElement.parentElement.dataset.id;
     deletepost(blogId);
@@ -25,20 +25,20 @@ document.addEventListener("click", (e) => {
 });
 
 const createBlog = (blogObj, endpoint, method) => {
-  const blogCard = document.createElement("div");
-  const blogHeader = document.createElement("div");
-  const blogTitle = document.createElement("h3");
-  const blogBody = document.createElement("div");
-  const blogBlock = document.createElement("blockquote");
-  const blogParagraph = document.createElement("q");
-  const publishedBy = document.createElement("p");
+  const blogCard = document.createElement('div');
+  const blogHeader = document.createElement('div');
+  const blogTitle = document.createElement('h3');
+  const blogBody = document.createElement('div');
+  const blogBlock = document.createElement('blockquote');
+  const blogParagraph = document.createElement('q');
+  const publishedBy = document.createElement('p');
 
-  blogCard.setAttribute("data-id", blogObj.id);
-  blogCard.className = "blog";
-  blogHeader.className = "blog-header";
-  blogTitle.className = "title";
-  blogBody.className = "blog-body";
-  publishedBy.className = "by";
+  blogCard.setAttribute('data-id', blogObj.id);
+  blogCard.className = 'blog';
+  blogHeader.className = 'blog-header';
+  blogTitle.className = 'title';
+  blogBody.className = 'blog-body';
+  publishedBy.className = 'by';
 
   fetchData(endpoint, method)
     .then((data) => {
@@ -56,26 +56,26 @@ const createBlog = (blogObj, endpoint, method) => {
   blogCard.appendChild(blogHeader);
   blogCard.appendChild(blogBody);
 
-  if (method === "POST") createDeleteBtn(blogHeader);
+  if (method === 'POST') createDeleteBtn(blogHeader);
 
   blogTitle.textContent = blogObj.title;
   blogParagraph.textContent = blogObj.content;
   blogsContainer.appendChild(blogCard);
 };
 
-logoutBtn.addEventListener("click", (e) => {
+logoutBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
-  fetchData("/logout", "POST")
+  fetchData('/logout', 'POST')
     .then((data) => {
-      if (data.message === "You are logged out") {
-        window.location.href = "/";
+      if (data.message === 'You are logged out') {
+        window.location.href = '/';
       }
     })
     .catch(console.log);
 });
 
-addBlogForm.addEventListener("submit", (e) => {
+addBlogForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const data = {
     blogTitle: addBlogForm.blogTitle.value,
@@ -83,30 +83,30 @@ addBlogForm.addEventListener("submit", (e) => {
   };
 
   if (data.blogTitle.length > 0 && data.blogContent.length > 0) {
-    fetchData("/add-blog", "POST", data).then((result) => {
+    fetchData('/add-blog', 'POST', data).then((result) => {
       if (
         result &&
-        result.message === "Title length must be at least 3 characters long"
+        result.message === 'Title length must be at least 3 characters long'
       ) {
         alert(result.message);
       } else {
         const blogData = result[0];
-        createBlog(blogData, "/username", "POST");
+        createBlog(blogData, '/username', 'POST');
       }
     });
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetchData("/username", "POST")
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData('/username', 'POST')
     .then((data) => {
       welcome.textContent = `Welcome ${data.name}`;
     })
     .catch(console.log);
-  fetchData("/all-blogs", "GET")
+  fetchData('/all-blogs', 'GET')
     .then((data) => {
       data.forEach((blog) => {
-        createBlog(blog, `/username?id=${blog.user_id}`, "GET");
+        createBlog(blog, `/username?id=${blog.user_id}`, 'GET');
       });
     })
     .catch(console.log);
